d47fb8ed1c586e6a6e2c4f6d71bf5cc08c9b0c8c
Rafael Kazuhito Vidal Beltrão <115881851+rkazuhito@users.noreply.github.com>
2026-04-09 11:00:44 -0300

    Add files via upload

--- desafio78.py
+++ desafio78.py
@@ -1,19 +1,20 @@
 lista=[]
-maior=0
-posmen=[]
-posmai=[]
+maior=menor=posmen=posmai=0
 for c in range(0,5):
     lista.append(int(input(f'Digite o valor da posição {c}: ')))
     if c==0:
         menor=lista[c]
-        posmen.append(c)
     else:
         if lista[c]<menor:
             menor=lista[c]
-            posmen.append(c)
     if lista[c]>maior:
         maior=lista[c]
-        posmai.append(c)
 print(f'A lista digitada ficou assim: {lista}')
-print(f'O maior valor digitado foi o {maior} na posição {posmai}')
-print(f'O menor valor digitado foi o {menor} na posição {posmen}')
+print(f'O maior valor digitado foi o {maior} na(s) posição(ões) ->', end=' ')
+for indice, valor in enumerate(lista):
+    if maior==valor:
+        print(f'{indice}', end=' ')
+print(f'\nO menor valor digitado foi o {menor} na(s) posição(ões) ->', end=' ')
+for indice,valor in enumerate(lista):
+    if menor==valor:
+        print(f'{indice}', end=' ')
